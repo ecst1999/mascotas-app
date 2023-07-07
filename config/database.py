aa1d7c0bc940a5bb35273694d8254d03442eb7dd
ecst1999 <54187718+ecst1999@users.noreply.github.com>
2023-07-07 10:29:16 -0500

    Cambio url conn

--- config/database.py
+++ config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("postgres://api:2eGt8yXoPI94AkN1QsMFI4iCgkjUs97R@dpg-cik2b7unqql0l1v86i20-a/api_v9fb", echo=True)
+engine = create_engine("postgresql+pg8000://api:2eGt8yXoPI94AkN1QsMFI4iCgkjUs97R@dpg-cik2b7unqql0l1v86i20-a/api_v9fb", echo=True)
 
 Session = sessionmaker(bind=engine)
 
