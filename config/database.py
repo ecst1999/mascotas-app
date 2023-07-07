6a79d163f07b6e600154928ff76d8f30a9111fbf
ecst1999 <54187718+ecst1999@users.noreply.github.com>
2023-07-07 10:43:23 -0500

    Data

--- config/database.py
+++ config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("postgresql://api:2eGt8yXoPI94AkN1QsMFI4iCgkjUs97R@dpg-cik2b7unqql0l1v86i20-a/api_v9fb", echo=True)
+engine = create_engine("postgresql://....", echo=True)
 
 Session = sessionmaker(bind=engine)
 
