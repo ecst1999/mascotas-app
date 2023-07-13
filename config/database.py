2da4746d4f9bbdd6a6218344813939b521add4a5
ecst1999 <54187718+ecst1999@users.noreply.github.com>
2023-07-13 18:32:35 -0500

    New database

--- config/database.py
+++ config/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # engine = create_engine("mysql+pymysql://api:12345@localhost:3307/api?charset=utf8mb4", echo=True)
-engine = create_engine("postgresql://api:2eGt8yXoPI94AkN1QsMFI4iCgkjUs97R@dpg-cik2b7unqql0l1v86i20-a/api_v9fb", echo=True)
+engine = create_engine("postgresql://mascotas_app_user:vUepE4OVRSkA3wUYRjWeU5PXDc71rjVe@dpg-cio8hnp8g3n6h7j709t0-a/mascotas_app", echo=True)
 
 Session = sessionmaker(bind=engine)
 
