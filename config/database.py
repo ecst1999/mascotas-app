b1b317e264f835ab886a21da9168bd4e1a5420c9
ecst1999 <54187718+ecst1999@users.noreply.github.com>
2023-07-15 13:38:40 -0500

    Role on

--- config/database.py
+++ config/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("mysql+pymysql://api:12345@localhost:3307/api?charset=utf8mb4", echo=True)
-# engine = create_engine("postgresql://mascotas_app_user:vUepE4OVRSkA3wUYRjWeU5PXDc71rjVe@dpg-cio8hnp8g3n6h7j709t0-a/mascotas_app", echo=True)
+# engine = create_engine("mysql+pymysql://api:12345@localhost:3307/api?charset=utf8mb4", echo=True)
+engine = create_engine("postgresql://mascotas_app_user:vUepE4OVRSkA3wUYRjWeU5PXDc71rjVe@dpg-cio8hnp8g3n6h7j709t0-a/mascotas_app", echo=True)
 
 Session = sessionmaker(bind=engine)
 
